91aaa8844f8d4070303aa34049f15c79b21e12bc
Long Tran <longdtranw@gmail.com>
2023-03-19 22:00:24 -0600

    attempt for recursion segmenation but failed

--- split.py
+++ split.py
@@ -7,10 +7,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# EMPTY THRESHOLD
-# the sum of either horizontal or vertical that is greater than this means that it is empty
-EMPTY_THRESHOLD = 10
+# segmentize image
+# not quite recursive right now because the recursion does not work. 
+def segmentize_recursive(img):
+    img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    imgs = segmentize(add_padding(black_or_white_transformer(img)))
+    return imgs
 
+# 0 if space
+# not 0 if not space. 
 def get_horizontal_hist(img, col):
     # If the img has foreground pixel as black i.e pixel value = 0
     horizontal_hist = col - np.sum(img,axis=1,keepdims=True)/255
@@ -31,19 +36,24 @@ def img_empty(img):
     # vertical hist
     horizontal_hist = get_horizontal_hist(img, col)
     
-    # sum all the values in the matrix
     result = np.sum(horizontal_hist)
-    if result > EMPTY_THRESHOLD:
+    if np.uint8(result) == 0:
         return True
-
+    
     return False
+
+def add_padding(img):
+    row, col= get_shape(img)
+    temp1 = np.array([[np.uint8(255) for i in range(col + 2)] for j in range(row + 2)])
+    temp1[1:row+1, 1:col+1] = img
+    return temp1
 
 def get_shape(img):
     return img.shape
 
 def segmentize(img):
      # the rgb of white is (255, 255, 255)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_img = img#cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_row = len(gray_img)
     img_col =  len(gray_img[0])
 
@@ -61,13 +71,13 @@ def segmentize(img):
     for i in range(len(horizontal_hist)):
         value = horizontal_hist[i][0]
 
-        if space_found and value > EMPTY_THRESHOLD and i > 0:
-            cv2.line(src, (0, i-1), (img_col, i-1), (255, 0, 0))
+        if space_found and value > 0 and i > 0:
+            #cv2.line(src, (0, i-1), (img_col, i-1), (255, 0, 0))
             rows.append(i-1)
             space_found = False
         elif value == 0:
             if not space_found:
-                cv2.line(src, (0, i), (img_col, i), (255, 0, 0))
+                #cv2.line(src, (0, i), (img_col, i), (255, 0, 0))
                 rows.append(i)
             space_found = True
             
@@ -77,13 +87,13 @@ def segmentize(img):
     space_found = False
     for i in range(len(vertical_hist[0])):
         value = vertical_hist[0][i]
-        if space_found and value > EMPTY_THRESHOLD and i > 0:
-            cv2.line(src, (i-1, 0), (i-1, img_row), (255, 0, 0))
+        if space_found and value > 0 and i > 0:
+            #cv2.line(src, (i-1, 0), (i-1, img_row), (255, 0, 0))
             cols.append(i-1)
             space_found = False
         elif value == 0:
             if not space_found:
-                cv2.line(src, (i, 0), (i, img_row), (255, 0, 0))
+                #cv2.line(src, (i, 0), (i, img_row), (255, 0, 0))
                 cols.append(i)
             space_found = True
 
@@ -95,8 +105,11 @@ def segmentize(img):
             row_e = rows[i+1]
             col_s = cols[j]
             col_e = cols[j+1]
+            
+            temp =  gray_img[row_s:row_e, col_s:col_e]
+            temp = add_padding(temp)
             sub_imgs.append(
-                gray_img[row_s:row_e, col_s:col_e]
+               temp
             )
 
     # check for empty images. 
@@ -105,7 +118,7 @@ def segmentize(img):
         shape = get_shape(img)
         if shape[0] == 0 or shape[1] == 0:
             continue
-        if not img_empty(img):
+        if img_empty(img):
             continue
             
         result.append(img)
@@ -113,32 +126,59 @@ def segmentize(img):
     # Plot histogram
     # plt.plot(horizontal_hist, range(img_row))
     # plt.xlim(img_col)
-    # plt.xlabel('Rows')
+    # plt.xlabel('aaaRows')
     # plt.ylabel('Frequency')
     # plt.show()
 
     return result
 
+# segmentize recursive
+def segmentize_recur(img, old_img=np.array([[]])):
+    if get_shape(img) == get_shape(old_img):
+        return img
     
+    sub_imges = segmentize(img)
+    result = []
+    for sub_img in sub_imges:
+        result.extend(segmentize_recur(sub_img, img))
+        
+        
+    
+    return result
 
+# pre-process the pixels
+def black_or_white_transformer(img):
+    for i in range(len(img)):
+        row = img[i]
+        for j in range(len(row)):
+            black_diff = np.abs(row[j] - 0)
+            white_diff = np.abs(row[j] - 255)
+            
+            row[j] = 0
+            if black_diff > white_diff:
+                row[j] = np.uint8(255)
+
+    return img
 if __name__ == "__main__":
     src = cv2.imread("test.png")
+    img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Source image', black_or_white_transformer(img))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
   
-
     if src is None:
         print('Could not open or find the image.')
         exit(0)
-   
-    imgs = segmentize(src)
+    
+    imgs = segmentize_recursive(src)
     print("sub images:", len(imgs))
     
-    cv2.imshow('Source image', src)
+  
     
     i = 0
     for img in imgs:
-        print(img.shape)
+      
         cv2.imshow(f'cropped{i}', img)
         i = i + 1
-       
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()   
