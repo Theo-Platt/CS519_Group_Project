d3bbcd75f5b61001ebcd8bae98261f49900ebefd
Long Tran <=>
2023-04-06 13:11:24 -0600

    add base class for DNN classifier

---
+++ train_codes/models.py
@@ -0,0 +1,15 @@
+class DNNClassifierBase:
+    def __init__():
+        pass
+    
+    # fit the model to the data
+    # inptut:
+    # + X: A NumPy 2D matrix containing the features fo each instance. 
+    # + y: A NumPy vector containing the class labels. 
+    def fit(X, y):
+        pass
+
+    # transform the data. 
+    # input: Expecting a NumPy 2D matrix containing the features of each instance. 
+    def transform(X):
+        pass
