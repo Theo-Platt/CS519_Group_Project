d209e1f6232ea21ed7d71fa880e9f3ff62d49d6a
Theo-Platt <theop@nmsu.edu>
2023-04-23 16:57:20 -0600

    running tests

--- settings/CONFIG.py
+++ settings/CONFIG.py
@@ -71,8 +71,8 @@ TEST_1 = ("2 x - 3 = - 7",                # actual symbols in image
           [1,3,2,1,2,2,1],                # correct model to classify symbols by
           Path('./eval_codes/test1.png')) # path to test1.png image
 
-TEST_2 = ("1 2 3 4 5 6 7 8 9 1 0 1 1 1 2 1 4", # actual symbols in image 
-          [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], # correct model to classify symbols by
+TEST_2 = ("1 2 3 4 5 6 7 8 9 1 0 1 1 1 2 1 4 1", # actual symbols in image 
+          [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], # correct model to classify symbols by
           Path('./eval_codes/test2.png'))      # path to test2.png image
 
 TEST_3 = ("3 + 4 - 5 X a 5 + a x B",      # actual symbols in image 
